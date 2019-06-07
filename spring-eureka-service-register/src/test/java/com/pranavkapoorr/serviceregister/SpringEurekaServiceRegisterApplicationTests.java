11c1a76a67a0d94e2b642844a97c8c7fe49aec18
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-06-07 11:38:38 -0400

    added eureka

---
+++ spring-eureka-service-register/src/test/java/com/pranavkapoorr/serviceregister/SpringEurekaServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavkapoorr.serviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- spring-name-to-greet/src/main/java/com/pranavkapoorr/greeting/me/GreetMeController.java
+++ spring-name-to-greet/src/main/java/com/pranavkapoorr/greeting/me/GreetMeController.java
@@ -1,20 +1,31 @@
 package com.pranavkapoorr.greeting.me;
 
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.*;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class GreetMeController {
 	
 	private static final Logger log = LoggerFactory.getLogger(GreetMeController.class);
-
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
 	@GetMapping("/greet/{name}")
 	public String greetMe(@PathVariable String name){
-		String greeting = consumeGreetingService("http://localhost:40001/greeting");
+		List<ServiceInstance> instances=discoveryClient.getInstances("greeting-producer");
+		ServiceInstance serviceInstance=instances.get(0);
+		String baseUrl=serviceInstance.getUri().toString();
+		String greeting = consumeGreetingService(baseUrl+"/greeting");
 		return greeting + " " + name;
 	}
 	
